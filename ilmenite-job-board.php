d4c569a2af102b173eec5c9c87e57a94fd0913f8
Erik Bernskiold <erik@xldstudios.com>
2013-07-24 10:45:51 +0200

    Initial commit
    Signed-off-by: Erik Bernskiold <erik@xldstudios.com>

---
+++ ilmenite-job-board.php
@@ -0,0 +1,83 @@
+<?php
+/**
+ * Plugin Name: Ilmenite Job Board
+ * Plugin URI: https://github.com/ErikBernskiold/Ilmenite-Job-Board
+ * Author: XLD Studios
+ * Author URI: http://www.xldstudios.com/
+ * Description: A Job Board for WordPress.
+ * Version: 1.0
+ * Requires at least: 3.5
+ * Tested up to: 3.5.2
+ * License: GPL2
+ */
+
+// Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) )
+	exit;
+
+/**
+ * Ilmenite Job Board Class
+ */
+class Ilmenite_Job_Board {
+
+	/**
+	 * Constructor
+	 */
+	public function __construct() {
+
+		// Define some constants
+		define( 'IL_JOB_BOARD_VERSION', '1.0' );
+		define( 'IL_JOB_BOARD_PLUGIN_DIR', untrailingslashit( plugin_dir_path( __FILE__ ) ) );
+		define( 'IL_JOB_BOARD_PLUGIN_URL', untrailingslashit( plugins_url( basename( plugin_dir_path( __FILE__ ) ), basename( __FILE__ ) ) ) );
+
+		// Add the textdomain and support translation
+		add_action( 'plugins_loaded', array( $this, 'add_textdomain' ) );
+
+		// Add plugin updater
+		add_action( 'init', array( $this, 'plugin_update' ) );
+	}
+
+	/**
+	 * Add textdomain for plugin
+	 */
+	public function add_textdomain() {
+		load_plugin_textdomain( 'iljobboard', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
+	}
+
+	/**
+	 * Auto Update Support
+	 *
+	 * Adds support for auto-updating from GitHub repository.
+	 */
+	public function plugin_update() {
+
+		// Include updater class
+		include( 'inc/class-github-updater.php' );
+
+		define( 'WP_GITHUB_FORCE_UPDATE', true );
+
+		if ( is_admin() ) { // note the use of is_admin() to double check that this is happening in the admin
+
+			$config = array(
+				'slug'               => plugin_basename( __FILE__ ),
+				'proper_folder_name' => 'ilmenite-slider',
+				'api_url'            => 'https://api.github.com/repos/xldstudios/ilmenite-slider',
+				'raw_url'            => 'https://raw.github.com/xldstudios/ilmenite-slider/master',
+				'github_url'         => 'https://github.com/xldstudios/ilmenite-slider',
+				'zip_url'            => 'https://github.com/xldstudios/ilmenite-slider/archive/master.zip',
+				'sslverify'          => true,
+				'requires'           => '3.0',
+				'tested'             => '3.6',
+				'readme'             => 'README.md',
+			);
+
+			new WP_GitHub_Updater( $config );
+
+		}
+
+	}
+
+}
+
+// Initialize everything
+$GLOBALS['ilmenite_job_board'] = new Ilmenite_Job_Board();
