010a11f32ada95c827771ee0b88d9c32e0e319e7
Erik Bernskiold <erik@xldstudios.com>
2013-07-24 11:37:36 +0200

    Display Job List Shortcode
    Signed-off-by: Erik Bernskiold <erik@xldstudios.com>

--- inc/class-ilmenite-job-board-shortcodes.php
+++ inc/class-ilmenite-job-board-shortcodes.php
@@ -16,11 +16,54 @@ class Ilmenite_Job_Board_Shortcodes {
 
 	}
 
-	public function jobs_listing() {
+	public function jobs_listing( $atts ) {
 
-		$output = 'Hej!';
+		ob_start();
 
-		return $output;
+		// Extract the attributes in the shortcode to own variables
+		extract( shortcode_atts( array(
+			'amount'       => 20,
+			'orderby'      => 'date',
+			'order'        => 'desc',
+			'show_filters' => true,
+		), $atts ) );
+
+		// Query Arguments
+		$jobs_args = array(
+			'post_type'           => 'il_job_board',
+			'posts_per_page'      => $amount,
+			'orderby'             => $orderby,
+			'order'               => $order,
+			'ignore_sticky_posts' => 1,
+			'post_status'         => 'publish',
+/*			'meta_query' 			 => array(
+				array(
+					'key'     => 'il_job_board_filled',
+					'value'   => 1,
+					'compare' => '!=',
+				),
+			),*/
+		);
+
+		$jobs = new WP_Query( $jobs_args );
+
+		if ( $jobs->have_posts() ) : ?>
+
+			<ul>
+
+				<?php while ( $jobs->have_posts() ) : $jobs->the_post(); ?>
+
+					<li><?php the_title(); ?></li>
+
+				<?php endwhile; ?>
+
+			</ul>
+
+		<?php endif;
+
+		wp_reset_postdata();
+
+		return '<div class="job-listing">' . ob_get_clean() . '</div>';
 
 	}
 
