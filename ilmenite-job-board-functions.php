b64aeeec3ed99fd5389dfaddb641a0cc8201713b
Erik Bernskiold <erik@xldstudios.com>
2013-07-25 15:31:15 +0200

    Added job taxonomy function
    Gets taxonomy terms with pre-defined defaults.
    
    Signed-off-by: Erik Bernskiold <erik@xldstudios.com>

--- ilmenite-job-board-functions.php
+++ ilmenite-job-board-functions.php
@@ -3,6 +3,7 @@
  * General Functions
  */
 
+if ( ! function_exists( 'iljb_get_message' ) ) :
 /**
  * Message Function
  */
@@ -26,3 +27,24 @@ function iljb_get_message( $type = '', $content = '' ) {
 	return '<div class="job-board-message ' . $class . '">' . $content . '</div>';
 
 }
+endif;
+
+if ( ! function_exists( 'get_job_taxonomy' ) ) :
+/**
+ * Retrieve Job Taxonomies
+ *
+ * Helper class to simply get the taxonomy terms from a
+ * taxonomy with pre-set options for the fitlering/submission.
+ */
+function get_job_taxonomy( $taxonomy = null ) {
+
+	$terms = get_terms( $taxonomy, array(
+		'orderby'    => 'name',
+		'order'      => 'asc',
+		'hide_empty' => false,
+	));
+
+	return $terms;
+
+}
+endif;
